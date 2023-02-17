23d19be6547d36e551d45cd32c1d6a775ba6cd9c
aqhafis <hafis.firosh@rakbank.ae>
2023-02-17 17:29:03 +0400

    fixes

--- server.js
+++ server.js
@@ -9,42 +9,42 @@ const cors = require("cors");
 const app = express() // initializing express framework
 app.use(cors());
 //config express.js
-app.use(express.json()) 
-app.set('port',3000) // setting the port to 3000
-app.use((req,res,next)=>{
-    res.setHeader('Access-Control-Allow-Origin','*');
+app.use(express.json())
+app.set('port', 3000) // setting the port to 3000
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
     next();
 })
 
 // connecting the database to the lessons_db database
 let db;
-MongoClient.connect('mongodb+srv://hafis:hafis123@cluster0.jrmnn1c.mongodb.net',(err,client)=>{
+MongoClient.connect('mongodb+srv://hafis:hafis123@cluster0.jrmnn1c.mongodb.net', (err, client) => {
     db = client.db('Project');
 })
 
 // logger middleware which outputs all requests to the server
-let logger = (req,res,next) =>{
+let logger = (req, res, next) => {
     let current_datetime = new Date();
     let formatted_date =
-      current_datetime.getFullYear() +
-      "-" +
-      (current_datetime.getMonth() + 1) +
-      "-" +
-      current_datetime.getDate() +
-      " " +
-      current_datetime.getHours() +
-      ":" +
-      current_datetime.getMinutes() +
-      ":" +
-      current_datetime.getSeconds();
-      let method = req.method;
-      let url = req.url;
-      let status = res.statusCode;
-      let log = `[${formatted_date}] ${method}:${url} ${status}`;
-      console.log(log);
-      next();
-    };
-    
+        current_datetime.getFullYear() +
+        "-" +
+        (current_datetime.getMonth() + 1) +
+        "-" +
+        current_datetime.getDate() +
+        " " +
+        current_datetime.getHours() +
+        ":" +
+        current_datetime.getMinutes() +
+        ":" +
+        current_datetime.getSeconds();
+    let method = req.method;
+    let url = req.url;
+    let status = res.statusCode;
+    let log = `[${formatted_date}] ${method}:${url} ${status}`;
+    console.log(log);
+    next();
+};
+
 app.use(logger);
 
 app.use('/images', function (req, res, next) {
@@ -64,28 +64,28 @@ app.use('/images', function (req, res, next) {
 
 
 // GET request for the the user enters : "localhost:3000"
-app.get('/',(req,res,next)=>{
+app.get('/', (req, res, next) => {
     res.send('Select a collection, e.g., /collection/messages');
 });
 
 //setting up the parameters for the REST API 
-app.param('collectionName',(req,res,next,collectionName)=>{
+app.param('collectionName', (req, res, next, collectionName) => {
     req.collection = db.collection(collectionName);
     return next();
 })
 
 // Rest API Task : GET Route to return all lessons
 //Get request to GET all lessons and their information
-app.get('/collection/:collectionName',(req,res,next)=>{
-    req.collection.find({}).toArray((e,results)=>{
-        if(e) return next(e)
+app.get('/collection/:collectionName', (req, res, next) => {
+    req.collection.find({}).toArray((e, results) => {
+        if (e) return next(e)
         res.send(results);
     })
-}) 
+})
 
 // REST API Task : POST route that saves a new order to the "order" collection
-app.post('/collection/:collectionName',(req,res,next)=>{
-    req.collection.insertOne(req.body,(e,results)=>{
+app.post('/collection/:collectionName', (req, res, next) => {
+    req.collection.insertOne(req.body, (e, results) => {
         if (e) return next(e)
         res.send(results.ops);
     })
@@ -93,34 +93,34 @@ app.post('/collection/:collectionName',(req,res,next)=>{
 
 //retrieve an object by mongodb ID
 
-app.get('/collection/:collectionName/:id',(req,res,next)=>{
+app.get('/collection/:collectionName/:id', (req, res, next) => {
     req.collection.findOne(
-        {_id:new ObjectID(req.params.id)},
-        (e,result)=>{
-            if(e) return next(e)
-                res.send(result);
-        
-    })
+        { _id: new ObjectID(req.params.id) },
+        (e, result) => {
+            if (e) return next(e)
+            res.send(result);
+
+        })
 })
 
 
 
 // REST API Task : PUT route that updates the number of available spaces in the ‘lesson’ collection
 // REST API Task: PUT route that updates the number of available spaces in the ‘lesson’ collection
-app.put('/collection/:collectionName/:id',(req, res)=>{
+app.put('/collection/:collectionName/:id', (req, res) => {
 
-    req.collection.update({_id:new ObjectId(req.params.id)},
-    {$set:req.body},
-    {safe:true,multi:false},
-    (e,result)=>{
-        if(e)return next(e)
-        res.send((result = 1)?{msg:"success"}:{msg:'error'})
-    })
+    req.collection.update({ _id: new ObjectId(req.params.id) },
+        { $set: req.body },
+        { safe: true, multi: false },
+        (e, result) => {
+            if (e) return next(e)
+            res.send((result = 1) ? { msg: "success" } : { msg: 'error' })
+        })
 
 });
 
 
 //app.listen() binds and listens the connections on the specified host and port.
-app.listen(3000,()=>{
+app.listen(3000, () => {
     console.log('Express.js server running at localhost:3000');
 })
